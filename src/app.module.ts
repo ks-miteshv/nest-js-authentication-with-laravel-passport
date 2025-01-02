d90de7495a42c8d7d7ceca4717ff25504229e6a6
ks-miteshv <mitesh.viras@kalkani.in>
2025-01-02 12:20:20 +0530

    add configuration validation with Joi in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        HOST: Joi.string().optional(),
+        PORT: Joi.number().optional(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
