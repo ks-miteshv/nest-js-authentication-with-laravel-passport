83aec9e5e867859d4115d176b61b84f60f1239c5
ks-miteshv <mitesh.viras@kalkani.in>
2025-01-02 12:21:50 +0530

    remove unused ConfigService import from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
